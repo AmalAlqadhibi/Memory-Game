46857930c0508c43cb09a2a1de5ed256c74b1eb3
Amal Alqadhibi <amalqudaibi@gmail.com>
2019-10-02 03:18:26 +0300

    add all requerment

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
-
 //Create a list that holds all of your cards
 const cards = document.querySelectorAll('.card');
 let playerMoves = document.querySelector('.moves');
+let timer = document.querySelector("#timer")
 const restartButton = document.querySelector('.restart');
+const stars = document.querySelector('.stars');
 let cardName = [];
 let openCard = [];
+let openedCard = [];
 let movment = 0;
+let timerFlag;
+//for timer
+var input = {
+    minutes: 10,
+    seconds: 30
+};
+let timestamp = new Date(input.minutes, input.seconds);
+let interval = 1;
+//start the init setting
 resetGame();
 
 function resetGame() {
-     cardName = [];
-     openCard = [];
+    movment = 0;
+    timestamp = new Date(input.minutes, input.seconds);
+    cardName = [];
+    openCard = [];
+    openedCard = [];
     playerMoves.textContent = 0;
+    timer.textContent = "0m:0s";
+    stars.innerHTML = `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+    stopTimer();
     cards.forEach(function(card) {
         card.classList.remove("open", "show", "match");
         let child = card.children[0];
@@ -44,21 +63,15 @@ function shuffleCard() {
 }
 
 function isMatched(card) {
-
     if (openCard[1].children[0].className == openCard[0].children[0].className) {
         openCard[0].classList.add('open', 'match');
         openCard[1].classList.add('open', 'match');
+        openedCard.push(openCard[0],openCard[1])
         $(document).ready(function() {
-
             $(document).ready(function() {
-                $(openCard[0]).effect("highlight", {
-                    times: 3
-                });
-                $(openCard[1]).effect("highlight", {
-                    times: 3
-                });
+                $(openCard[0]).effect("highlight", {times: 3});
+                $(openCard[1]).effect("highlight", {times: 3});
             });
-
         });
     } else {
         $(document).ready(function() {
@@ -69,29 +82,54 @@ function isMatched(card) {
 };
 
 cards.forEach(function(card) {
-    card.addEventListener("click", function() {
 
+    card.addEventListener("click", function() {
+        startTimer();
+    if (openedCard.length != 16){
         if (!card.classList.contains("open")) {
-            movment += 1;
-            playerMoves.textContent = movment
+setScorePanel();
             openCard.push(card);
             if (openCard.length <= 2) {
                 card.classList.add('open', 'show');
                 setTimeout(function() {
                     card.classList.remove('open', 'show');
                     openCard = [];
-                }, 1300);
-
+                }, 1100);
             }
             if (openCard.length == 2) {
                 isMatched(card)
             }
         }
+    }
+     if (openedCard.length == 16) {
+        stopTimer();
+        let dialog = document.querySelector("#score").textContent= `With ${movment} Moves and ${stars.childElementCount} Stars`;
+        document.getElementById("WinningDialog").showModal();
+    }
     });
 });
-
+function startTimer(){
+    if (!timerFlag) {
+    timerFlag = setInterval(function () {
+        timestamp = new Date(timestamp.getTime() + 1 * 1000);
+        document.getElementById('timer').innerHTML = 'm:' + timestamp.getSeconds() + 's';
+    }, Math.abs(1) * 1000);
+}}
+function stopTimer(){
+    clearInterval(timerFlag);
+    timerFlag = null;
+}
+function setScorePanel(){
+    movment += 1;
+    playerMoves.textContent = movment
+        if (movment == 22) {
+            stars.removeChild(stars.lastElementChild);
+        } else if (movment == 29) {
+              stars.removeChild(stars.lastElementChild);
+           }
+}
 function playAgainButton() {
-    document.getElementById("myDialog").close();
+    document.getElementById("WinningDialog").close();
     resetGame();
 }
 restartButton.addEventListener("click",resetGame)
